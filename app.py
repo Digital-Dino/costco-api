3d3dd93ee5bca00c15febb3601953e6c40c6bd53
Digital-Dino <eliasrexes@gmail.com>
2023-09-29 22:20:58 -0700

    added create_app function

--- app.py
+++ app.py
@@ -3,17 +3,20 @@ from flask_smorest import Api
 
 from resources.costco import blp as CostcoBlueprint
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['API_TITLE'] = 'Costco API'
-app.config['API_VERSION'] = 'v1'
-app.config['OPENAPI_VERSION'] = '3.0.3'
-app.config['OPENAPI_URL_PREFIX'] = '/'
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+    app.config['API_TITLE'] = 'Costco API'
+    app.config['API_VERSION'] = 'v1'
+    app.config['OPENAPI_VERSION'] = '3.0.3'
+    app.config['OPENAPI_URL_PREFIX'] = '/'
 
-app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
-app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
+    app.config['OPENAPI_SWAGGER_UI_PATH'] = '/swagger-ui'
+    app.config['OPENAPI_SWAGGER_UI_URL'] = 'https://cdn.jsdelivr.net/npm/swagger-ui-dist/'
 
-api = Api(app)
+    api = Api(app)
 
-api.register_blueprint(CostcoBlueprint)
+    api.register_blueprint(CostcoBlueprint)
+
+    return app
